61d7ad82b8313c6009457707fd3e15cddd595c9c
Kevin Lu <astroca4@gmail.com>
2016-09-25 04:59:46 -0400

    Add files via upload
    Initial algorithm for pulling data from the Firebase database; currently ranks locations from lowest to highest volume; does not take into account user profile; all locations are hardcoded.

---
+++ ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  CookieQuiet
+//
+//  Created by Kevin Lu on 9/25/16.
+//  Copyright © 2016 Kevin Lu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // All the ids in the locations array must match the ids in the firebase database
+        let locations = ["aud", "r101", "r102", "r105"]
+        let names = ["Auditorium", "Room 101", "Room 102", "Room 103"]
+        // For each id in the locations array there must be a 0 in the ranks and sorted arrays
+        var ranks: [Int] = [0, 0, 0, 0]
+        var sorted: [Int] = [0, 0, 0, 0]
+        
+        let ref = FIRDatabase.database().reference()
+        
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            let dict = snapshot.value as? NSDictionary
+            
+            var i = 0
+            for location in locations {
+                //print(dict?[location])
+                let s = dict?[location] as! Int
+                print(s)
+                ranks[i] = s
+                sorted[i] = s
+                i += 1
+            }
+            
+            sorted.sort()
+            
+            for rank in 0...(locations.count - 1) {
+                for l in 0...(locations.count - 1) {
+                    if (sorted[rank] == ranks[l]) {
+                        // Set the next availiable table cell to names[l]
+                        // tableCellProtocol.image and stuff = ...
+                        // tableCellProtocol.text = names[l] etc.
+                        break;
+                    }
+                }
+            }
+        })
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
