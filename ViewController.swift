a0f92bc888d3b3ac34f5bbe98391fb4b793c14ed
jayczech23 <cech.jordan@gmail.com>
2016-09-25 09:55:54 -0400

    Add files via upload
    version 2.0

--- ViewController.swift
+++ ViewController.swift
@@ -7,82 +7,15 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
-   
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var playBtn: UIButton!
-    @IBOutlet weak var okayBtn: UIButton!
-    @IBOutlet weak var stopBtn: UIButton!
-    
-    var testSound: AVAudioPlayer!
-    
-    var soundToleranceValue: Float = 0
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let path = Bundle.main.path(forResource: "test", ofType: "mp3")
-        let soundURL = URL(fileURLWithPath: path!)
-        
-        do {
-            
-            try testSound = AVAudioPlayer(contentsOf: soundURL)
-            testSound.prepareToPlay()
-        } catch let err as NSError {
-            print(err.debugDescription)
-        }
+       
        
     }
-    
-    @IBAction func playBtnPressed(_ sender: AnyObject) {
-        playSound()
-        stopBtn.isHidden = false
-        
-    }
-    
-    func playSound() {
-        
-        if testSound.isPlaying {
-            testSound.stop()
-        }
-        
-        testSound.play()
-        testSound.numberOfLoops = 100
-        okayBtn.isHidden = false
-        playBtn.isHidden = true
-    }
-    
-    
-    @IBAction func okayBtnPressed(_ sender: AnyObject) {
-    
-        //okayBtn.isHidden = true
-        //playBtn.isHidden = false
-        testSound.stop()
-        
-        soundToleranceValue = (slider.value * 10) / 2
-        slider.isHidden = true
-        
-        // print to logs the value of the tolerance level.
-        print("Your tolerance level is: \(soundToleranceValue)")
-    }
-    
-    @IBAction func sliderModified(_ sender: AnyObject) {
-        
-        testSound.volume = slider.value
-        
-    }
-    
-    @IBAction func stopBtnPressed(_ sender: AnyObject) {
-        
-        testSound.stop()
-        playBtn.isHidden = false
-        stopBtn.isHidden = true
-        
-    }
-    
+
     
 
 }
